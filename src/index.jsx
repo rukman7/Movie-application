95eb0ee6f17557127f8621e39b33b28fbe2285f4
Rukmangathan <rukman@xyz.com>
2023-03-01 17:25:21 +0000

    Added routing and live data from API.

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import MovieDetailsPage from './pages/movieDetailsPage'
-import sample from './stories/sampleData'
-
-const movies = [sample, sample, sample, sample, sample, sample, sample];
-
-const images = [
-  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-]
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
 
 const App = () => {
-    return (
-      <MovieDetailsPage movie={sample} images={images} />
-    );
-  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
