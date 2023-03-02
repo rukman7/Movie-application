4d06ea6a91e1d120c821df6dd1fef39c4cd24180
Rukmangathan <rukman@xyz.com>
2023-03-02 15:30:05 +0000

    Added Upcoming movies page.

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
---
+++ src/pages/upcomingMoviesPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage'
+
+const UpcomingMoviesPage = (props) => {
+  const [movies, setMovies] = useState([]);
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
+  useEffect(() => {
+    getUpcomingMovies().then(movies => {
+      setMovies(movies);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  return (
+    <PageTemplate
+      title='Upcoming movies'
+      movies={movies}
+      selectFavourite={addToFavourites}
+    />
+  );
+};
+export default UpcomingMoviesPage;
