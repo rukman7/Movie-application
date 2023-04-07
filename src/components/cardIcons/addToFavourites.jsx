53ea1227124ce07fdf417f2c7188825c5f86303a
Rukmangathan <rukman@xyz.com>
2023-04-07 21:03:03 +0100

    Tagging upcoming movies as must-watch.

--- src/components/cardIcons/addToFavourites.jsx
+++ src/components/cardIcons/addToFavourites.jsx
@@ -3,23 +3,43 @@ import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import PlaylistAddCheckCircleIcon from "@mui/icons-material/PlaylistAddCheckCircle";
 
 const AddToFavouritesIcon = ({ movie, page = "" }) => {
-  const context = useContext(MoviesContext);
+  const {
+    addToFavourites,
+    addToMustWatchMovies,
+    removeFromMustWatchMovies,
+    isInMustWatchList,
+  } = useContext(MoviesContext);
 
   const onUserSelect = (e) => {
     e.preventDefault();
-    context.addToFavourites(movie);
+    addToFavourites(movie);
+  };
+
+  const onAddPlaylist = (e) => {
+    e.preventDefault();
+    addToMustWatchMovies(movie);
+  };
+
+  const onRemovePlaylist = (e) => {
+    e.preventDefault();
+    removeFromMustWatchMovies(movie);
   };
 
   if (page === "upcoming") {
-    return (
-      <IconButton aria-label="add to must watch">
+    return isInMustWatchList(movie) ? (
+      <IconButton aria-label="remove from mustwatch" onClick={onRemovePlaylist}>
+        <PlaylistAddCheckCircleIcon color="error" fontSize="large" />
+      </IconButton>
+    ) : (
+      <IconButton aria-label="add to mustwatch" onClick={onAddPlaylist}>
         <PlaylistAddIcon color="primary" fontSize="large" />
       </IconButton>
     );
   }
-  
+
   return (
     <IconButton aria-label="add to favorites" onClick={onUserSelect}>
       <FavoriteIcon color="primary" fontSize="large" />
--- src/contexts/moviesContext.jsx
+++ src/contexts/moviesContext.jsx
@@ -5,6 +5,7 @@ export const MoviesContext = React.createContext(null);
 const MoviesContextProvider = (props) => {
   const [myReviews, setMyReviews] = useState( {} )
   const [favourites, setFavourites] = useState([]);
+  const [mustWatchMovies, setMustWatchMovies] = useState([]);
 
   const addReview = (movie, review) => {   // NEW
     setMyReviews( {...myReviews, [movie.id]: review } )
@@ -18,10 +19,22 @@ const MoviesContextProvider = (props) => {
     setFavourites(updatedFavourites);
   };
 
-  // We will use this function in a later section
+  const addToMustWatchMovies = (movie) => {
+    let updatedMustWatchMovies = [...mustWatchMovies];
+    if (!mustWatchMovies.includes(movie.id)) {
+      updatedMustWatchMovies.push(movie.id);
+    }
+    setMustWatchMovies(updatedMustWatchMovies);
+  };
+
   const removeFromFavourites = (movie) => {
     setFavourites(favourites.filter((mId) => mId !== movie.id));
   };
+
+  const removeFromMustWatchMovies = (movie) =>
+  setMustWatchMovies(mustWatchMovies.filter((mId) => mId !== movie.id));
+
+  const isInMustWatchList = (movie) => mustWatchMovies.includes(movie.id);
 
   return (
     <MoviesContext.Provider
@@ -30,6 +43,9 @@ const MoviesContextProvider = (props) => {
         addToFavourites,
         removeFromFavourites,
         addReview,
+        addToMustWatchMovies,
+        removeFromMustWatchMovies,
+        isInMustWatchList,
       }}
     >
       {props.children}
