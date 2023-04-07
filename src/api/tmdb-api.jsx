31b5cb7f33376d6d682b7b5a34e5ce89703ba02a
Rukmangathan <rukman@xyz.com>
2023-04-07 12:00:11 +0100

    Fix upcoming movies page.

--- src/api/tmdb-api.jsx
+++ src/api/tmdb-api.jsx
@@ -77,10 +77,13 @@ export const getMovie = (args) => {
       });
   };
   
-  export const getUpcomingMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };  
+  export const getUpcomingMovies = (page) =>
+  fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`)
+      .then((response) => {
+          if (!response.ok) {
+              throw new Error(response.json().message);
+          }
+          return response.json();
+      }).catch((error) => {
+          throw error;
+      });
--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,33 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import PageTemplate from '../components/templateMovieListPage'
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { MoviesContext } from "../contexts/moviesContext";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favourites = movies.filter(m => m.favourite)
-  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const { pageUpcomingMovies, setPageUpcomingMovies } =
+    useContext(MoviesContext);
+  const { isLoading, isError, error, data, isFetching } = useQuery(
+    ["upcoming", pageUpcomingMovies],
+    () => getUpcomingMovies(pageUpcomingMovies)
+  );
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
 
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
+  if (isLoading || isFetching) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const changeFilterValues = (type, value) => {
+    const newf = { name: type, value: value };
+    const newFilters =
+      type === "title" ? [newf, filterValues[1]] : [filterValues[0], newf];
+    setFilterValues(newFilters);
   };
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  const movies = data ? data.results : [];
+  const displayedMovies = filterFunction(movies);
 
   return (
-    <PageTemplate
-      title='Upcoming movies'
-      movies={movies}
-      selectFavourite={addToFavourites}
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={displayedMovies}
+        action={(movie) => {
+          return <AddToFavouritesIcon movie={movie} page="upcoming" />;
+        }}
+        page={pageUpcomingMovies}
+        pageSetter={setPageUpcomingMovies}
+      />
+      <MovieFilterUI
+        filterInputChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
+
 export default UpcomingMoviesPage;
