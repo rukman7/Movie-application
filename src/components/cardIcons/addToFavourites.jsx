d1a918b89814c85d6065de0264d43b7c2704838c
Rukmangathan <rukman@xyz.com>
2023-04-07 12:10:51 +0100

    New button for Upcoming movies page.

--- src/components/cardIcons/addToFavourites.jsx
+++ src/components/cardIcons/addToFavourites.jsx
@@ -2,14 +2,24 @@ import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 
-const AddToFavouritesIcon = ({ movie }) => {
+const AddToFavouritesIcon = ({ movie, page = "" }) => {
   const context = useContext(MoviesContext);
 
   const onUserSelect = (e) => {
     e.preventDefault();
     context.addToFavourites(movie);
   };
+
+  if (page === "upcoming") {
+    return (
+      <IconButton aria-label="add to must watch">
+        <PlaylistAddIcon color="primary" fontSize="large" />
+      </IconButton>
+    );
+  }
+  
   return (
     <IconButton aria-label="add to favorites" onClick={onUserSelect}>
       <FavoriteIcon color="primary" fontSize="large" />
